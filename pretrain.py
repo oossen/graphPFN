d037bb1472ee08ce5391ac455c1cf59bb00a6ba8
Ole Ossen <ole.ossen@gmail.com>
2025-11-17 01:29:21 +0100

    posterior model

--- pretrain.py
+++ pretrain.py
@@ -15,7 +15,7 @@ from priors.observational_dataloader import ObservationalDataLoader
 from visualization.make_visualization import make_all
 
 
-from configs.default_configs import prior_config, training_config as args
+from configs.default_configs_posterior import prior_config, training_config as args
 
 
 device = get_default_device()
