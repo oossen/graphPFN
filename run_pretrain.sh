f301cda567905daa45fa90551a542fa0dd2242c9
Ole Ossen <ole.ossen@gmail.com>
2026-02-01 17:06:46 +0100

    just one pretrain script

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -27,7 +27,7 @@ conda activate graph-pfn
 # Running the job
 start=`date +%s`
 
-python pretrain_likelihood.py --cuda --wait-time 5
+python pretrain.py --cuda --wait-time 5
 
 end=`date +%s`
 runtime=$((end-start))
